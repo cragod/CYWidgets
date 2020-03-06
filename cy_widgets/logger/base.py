0ecaff09dd1405b06eccafe66f2bfda023908b0c
hyl <hyl4@meitu.com>
2020-03-06 13:43:48 +0800

    add recorder logger

--- cy_widgets/logger/base.py
+++ cy_widgets/logger/base.py
@@ -1,14 +1,21 @@
-class BaseLogger:
-    """日志类抽象"""
+from abc import ABC, abstractmethod
 
-    def log_simple_info(self, content):
+
+class RecorderBase(ABC):
+    """日志记录抽象"""
+
+    @abstractmethod
+    def record_simple_info(self, content):
         """简单信息日志"""
-        print(content)
+        NotImplementedError("Not Implemented")
 
-    def log_procedure(self, content):
+    @abstractmethod
+    def record_procedure(self, content):
         """过程日志"""
-        print(content, end='\n\n')
+        NotImplementedError("Not Implemented")
 
-    def log_exception(self, content):
+    @abstractmethod
+    def record_exception(self, content):
         """异常日志"""
-        print(content, end='\n\n')
+        # print(content, end='\n\n')
+        NotImplementedError("Not Implemented")
