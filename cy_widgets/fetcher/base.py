2b47818a9cd3179e9e14f2cea965098245abb290
黄云龙 <hyl4@meitu.com>
2020-03-10 18:09:56 +0800

    调整 ExchangeFetcher 逻辑

--- cy_widgets/fetcher/base.py
+++ cy_widgets/fetcher/base.py
@@ -1,57 +1,49 @@
 import re
 import time
 import pandas as pd
-from abc import abstractproperty
 
 from cy_components.utils.one_token import OneToken
 from cy_components.utils.coin_pair import CoinPair
 from cy_components.defines.enums import TimeFrame
 from cy_components.helpers.formatter import CandleFormatter as cf
 from cy_components.defines.column_names import *
-from ..exchange.base import ExchangeBase
+from ..exchange.provider import CCXTProvider
 
 
-class ExchangeFetcher(ExchangeBase):
-    """抓取数据基础类, ExchangeBase 的抽象方法继续往下传"""
+class ExchangeFetcher:
+    """抓取数据类, 统一流程"""
 
-    one_token = OneToken()
+    def __init__(self, ccxt_provider: CCXTProvider, one_token: OneToken):
+        # API 对象都用外部传入
+        self.__ccxt_provider = ccxt_provider
+        self.__one_token = one_token
 
-    def __init__(self, apiKey='', apiSecret='', params={}):
-        super().__init__(apiKey=apiKey, apiSecret=apiSecret, params=params)
-        self._process_one_token_cfgs(params)
-
-    @abstractproperty
-    def _ccxt_object_for_fetching(self):
-        """获取用于抓取数据的 CCXT 对象"""
-        raise NotImplementedError
-
-    def _process_one_token_cfgs(self, params={}):
-        """尝试配置 OneToken 对象"""
-        if 'one_token_cfgs' in params:
-            self.one_token = OneToken(params['one_token_cfgs'])
-
-    def _fetch_candle_data_by_ccxt_object(self, coin_pair: CoinPair, time_frame: TimeFrame, since_timestamp, limit, hour_offset=0, params={}):
+    def __fetch_candle_data_by_ccxt_object(self, coin_pair: CoinPair, time_frame: TimeFrame, since_timestamp, limit, hour_offset=0, params={}):
         """通过 CCXT 抓取数据，转为统一格式"""
-        data = self._ccxt_object_for_fetching.fetch_ohlcv(coin_pair.pair(), time_frame.value, since_timestamp, limit)
+        data = self.__ccxt_provider.ccxt_object_for_fetching.fetch_ohlcv(
+            coin_pair.pair(), time_frame.value, since_timestamp, limit)
         df = cf.convert_raw_data_to_data_frame(data, hour_offset=hour_offset)
         return df
 
-    def _fetch_candle_data_by_one_token(self, coin_pair: CoinPair, time_frame: TimeFrame, since_timestamp, limit, params={}):
+    def __fetch_candle_data_by_one_token(self, coin_pair: CoinPair, time_frame: TimeFrame, since_timestamp, limit, params={}):
         """通过 OneToken 抓取数据，转为统一格式"""
         exchange_name = re.sub('Fetcher$', '', self.__class__.__name__)
-        data = self.one_token.fetch_candle_data(exchange_name, coin_pair, time_frame, limit, since_timestamp)
+        data = self.__one_token.fetch_candle_data(exchange_name, coin_pair, time_frame, limit, since_timestamp)
         df = pd.DataFrame(data=data, columns=[COL_CANDLE_BEGIN_TIME, COL_OPEN, COL_HIGH, COL_LOW, COL_CLOSE,
                                               COL_VOLUME])
         return df
 
     # 公开的业务逻辑
 
-    def fetch_historical_candle_data(self, coin_pair: CoinPair, time_frame: TimeFrame, since_timestamp, limit, params={}):
-        """Fetch historical ohlcv, use ccxt object by default."""
-        return self._fetch_candle_data_by_ccxt_object(coin_pair, time_frame, since_timestamp, limit, params=params)
+    def fetch_historical_candle_data(self, coin_pair: CoinPair, time_frame: TimeFrame, since_timestamp, limit, params={}, by_ccxt=True):
+        """获取历史K线"""
+        if by_ccxt:
+            return self.__fetch_candle_data_by_ccxt_object(coin_pair, time_frame, since_timestamp, limit, params=params)
+        else:
+            return self.__fetch_candle_data_by_one_token(coin_pair, time_frame, since_timestamp, limit, params)
 
     def fetch_real_time_candle_data(self, coin_pair: CoinPair, time_frame: TimeFrame, limit, params={}):
-        """Fetch real time ohlcv, use ccxt object."""
+        """获取实时K线"""
         result_df = None
         # each time count
         fetch_lmt = 888
@@ -67,7 +59,8 @@ class ExchangeFetcher(ExchangeBase):
             # fetch
             earliest_ts = int(time.mktime(earliest_date.timetuple()))
             fetch_ts = earliest_ts - fetch_lmt * time_frame.time_interval(res_unit='s')
-            df = self._fetch_candle_data_by_ccxt_object(coin_pair, time_frame, fetch_ts * 1000, fetch_lmt, hour_offset=8)
+            df = self.__fetch_candle_data_by_ccxt_object(
+                coin_pair, time_frame, fetch_ts * 1000, fetch_lmt, hour_offset=8)
 
             # update to df
             if result_df is None:
