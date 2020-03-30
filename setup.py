e30312402548b638cd980d5a16c6edf09d8da2e1
hyl <hyl4@meitu.com>
2020-03-30 17:49:40 +0800

    update setup configuration

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + '\n\n',
     include_package_data=True,
     keywords='cy_widgets',
     name='cy_widgets',
