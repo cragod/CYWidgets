af32ba0687c7739eb18c2b86ecf6aa9fd56ce0e5
hyl <hyl4@meitu.com>
2020-08-22 21:33:43 +0800

    提取长日志方法到基类

--- cy_widgets/logger/base.py
+++ cy_widgets/logger/base.py
@@ -4,6 +4,8 @@ from abc import ABC, abstractmethod
 class RecorderBase(ABC):
     """日志记录抽象"""
 
+    _summary_log = ""
+
     @abstractmethod
     def record_simple_info(self, content):
         """简单信息日志"""
@@ -19,6 +21,22 @@ class RecorderBase(ABC):
         """异常日志"""
         NotImplementedError("Not Implemented")
 
+    @abstractmethod
+    def _record_summary_log(self):
+        NotImplementedError("Not Implemented")
+
+    def append_summary_log(self, content):
+        # 打印并加入到最终日志
+        self.record_simple_info(content)
+        self._summary_log = self._summary_log + content + '\n\n'
+
+    def record_summary_log(self, content=None):
+        """记录整体日志，后清空"""
+        if content is not None:
+            self.append_summary_log(content)
+        self._record_summary_log()
+        self._summary_log = ""
+
 
 class SimpleRecorder(RecorderBase):
     """简单打印"""
@@ -31,3 +49,6 @@ class SimpleRecorder(RecorderBase):
 
     def record_exception(self, content):
         print(content, end='\n\n')
+
+    def _record_summary_log(self):
+        print(self._summary_log, end='\n\n')
