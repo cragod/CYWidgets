457e0e8d355d5560ba5b6236c511db8ee7774ad1
黄云龙 <hyl4@meitu.com>
2020-12-14 00:15:27 +0800

    评价方法更新到完整版

--- cy_widgets/backtest/strategy.py
+++ cy_widgets/backtest/strategy.py
@@ -6,7 +6,7 @@ from ..strategy.exchange.base import BaseExchangeStrategy
 class StrategyBacktest:
     """单次回测逻辑"""
 
-    def __init__(self, df: pd.DataFrame, strategy: BaseExchangeStrategy, pos_func, evaluate_func, result_handler):
+    def __init__(self, df: pd.DataFrame, strategy: BaseExchangeStrategy, pos_func, evaluate_func, result_handler, context=None):
         """回测配置初始化
 
         Parameters
@@ -19,7 +19,7 @@ class StrategyBacktest:
             仓位方法
         evaluate_func : (pod_df) -> df_ev
             评估方法
-        result_handler : (df_ev, strategy, error_des) -> result_df
+        result_handler : (context, df_pos, df_ev, strategy, error_des) -> result_df
             结果处理
         """
         # def assert_param(tar, tar_type):
@@ -35,12 +35,13 @@ class StrategyBacktest:
         self.__pos_func = pos_func
         self.__evaluate_func = evaluate_func
         self.__result_handler = result_handler
+        self.__context = context
 
     def perform_test(self):
         """执行回测"""
         if self.__strategy.available_to_calculate(self.__df):
             signal_df = self.__strategy.calculate_signals(self.__df)
             pos_df = self.__pos_func(signal_df)
-            return self.__result_handler(self.__evaluate_func(pos_df), self.__strategy, None)
+            return self.__result_handler(self.__context, pos_df, self.__evaluate_func(pos_df.copy()), self.__strategy, None)
         else:
-            return self.__result_handler(None, 'K 线数量不够计算信号')
+            return self.__result_handler(self.__context, None, None, None, 'K 线数量不够计算信号')
