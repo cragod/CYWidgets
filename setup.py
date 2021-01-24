7ade36e97a89c2268b780a0c6004e8ae79fc235b
hyl <hyl4@meitu.com>
2021-01-24 14:26:28 +0800

    Bump version: 0.4.13 → 0.4.14

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cragod/CYWidgets',
-    version='0.4.13',
+    version='0.4.14',
     zip_safe=False,
 )
