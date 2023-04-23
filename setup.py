9a9a67d5298fe4e32c9419273f06cfa28b71b53a
Talon.Huang <talon.huang@ringcentral.com>
2023-04-23 10:38:47 +0800

    Bump version: 0.4.35 → 0.4.36

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cragod/CYWidgets',
-    version='0.4.35',
+    version='0.4.36',
     zip_safe=False,
 )
